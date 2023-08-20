e750fbbb23247c8578a8816bc5b20991bf647c64
Vlad Edelev <vlladedellev@gmail.com>
2023-08-20 17:51:32 +0300

    refactor: added some strings to constants

--- lib/data/data_source/remote/user_remote_data_source.dart
+++ lib/data/data_source/remote/user_remote_data_source.dart
@@ -3,6 +3,7 @@ import 'package:flutter_github_searcher/core/di/di.dart';
 import 'package:flutter_github_searcher/core/network/api_config.dart';
 import 'package:flutter_github_searcher/core/network/api_result.dart';
 import 'package:flutter_github_searcher/core/network/dio_client.dart';
+import 'package:flutter_github_searcher/core/utils/strings.dart';
 import 'package:flutter_github_searcher/data/data_source/remote/base_user_remote_data_source.dart';
 import 'package:flutter_github_searcher/data/model/user_model.dart';
 
@@ -15,23 +16,15 @@ class UserRemoteDataSource implements BaseUserRemoteDataSource {
       final response = await dioClient.dio.get(
         '${ApiConfig.users}/$nickname',
       );
-
-      print('first print:  ${response}');
-
       final data = response.data as Map<String, dynamic>;
-
-      print('second print: ${data.toString()}');
-
       final user = UserModel.fromJson(data);
-
-      print(user.toString());
 
       return ApiResult.success(user);
     } on DioException catch (e) {
       if (e.type == DioExceptionType.badResponse) {
-        return const ApiResult.failure('No such user');
+        return const ApiResult.failure(AppStrings.noSuchUser);
       } else {
-        return const ApiResult.failure('Connection error');
+        return const ApiResult.failure(AppStrings.connectionError);
       }
     }
   }
